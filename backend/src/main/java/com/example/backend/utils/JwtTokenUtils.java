825493d8783bc85c3bdabb69bd52f1306e2e0856
k9798909 <k9798909@gmail.com>
2023-11-24 11:38:19 +0800

    jwt key default

--- backend/src/main/java/com/example/backend/utils/JwtTokenUtils.java
+++ backend/src/main/java/com/example/backend/utils/JwtTokenUtils.java
@@ -25,7 +25,7 @@ public class JwtTokenUtils {
     private long expirationTime;
 
     public JwtTokenUtils(@Value("${jwt.expiration.time}") long expirationTime,
-            @Value("${jwt.secret.key}") String secretKey) {
+            @Value("${jwt.secret.key:1hGfRtZsL7pOqXyW9uBvNc6mKjEiAx3d2gVfRtZsL7pOqXyW9uBvNc6mKjEiAx3d2gVfRtZsL7pOqX}") String secretKey) {
         this.expirationTime = expirationTime;
         this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
     }
